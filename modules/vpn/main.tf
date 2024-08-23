690d117b43b4271accf8e378c6e55618ed89c5b5
Mohamed Sahraoui <Belhadj.boumid@gmail.com>
2024-08-23 15:28:22 +0100

    Update main.tf

--- modules/vpn/main.tf
+++ modules/vpn/main.tf
@@ -7,30 +7,34 @@ terraform {
   }
 }
 
-# Check for existing VPN Gateway
+# Check for existing VPN Gateway with the specific name
 data "aws_vpn_gateway" "existing" {
-  count = var.vpc_id != "" ? 1 : 0
+  filter {
+    name   = "tag:Name"
+    values = ["app-vpc-vgw"]
+  }
   filter {
     name   = "attachment.vpc-id"
     values = [var.vpc_id]
   }
 }
 
-# Create VPN Gateway
+# Create VPN Gateway only if one with the specific name doesn't exist
 resource "aws_vpn_gateway" "vgw" {
   count  = length(data.aws_vpn_gateway.existing) == 0 ? 1 : 0
   vpc_id = var.vpc_id
 
   tags = {
-    Name = "${var.vpc_name}-vgw"
+    Name = "app-vpc-vgw"
   }
 }
 
 # Use existing VGW if available, otherwise use the newly created one
 locals {
-  vgw_id = length(data.aws_vpn_gateway.existing) > 0 ? data.aws_vpn_gateway.existing[0].id : try(aws_vpn_gateway.vgw[0].id, "")
+  vgw_id = length(data.aws_vpn_gateway.existing) > 0 ? data.aws_vpn_gateway.existing.id : try(aws_vpn_gateway.vgw[0].id, "")
 }
 
+# Create Customer Gateway
 resource "aws_customer_gateway" "cgw" {
   bgp_asn    = var.bgp_asn
   ip_address = var.cgw_eip
@@ -41,8 +45,8 @@ resource "aws_customer_gateway" "cgw" {
   }
 }
 
+# Create VPN Connection
 resource "aws_vpn_connection" "main" {
-  count               = local.vgw_id != "" ? 1 : 0
   vpn_gateway_id      = local.vgw_id
   customer_gateway_id = aws_customer_gateway.cgw.id
   type                = "ipsec.1"
@@ -53,14 +57,14 @@ resource "aws_vpn_connection" "main" {
   }
 }
 
+# Create VPN Connection Route
 resource "aws_vpn_connection_route" "main" {
-  count                  = local.vgw_id != "" ? 1 : 0
   destination_cidr_block = var.client_vpc_cidr
-  vpn_connection_id      = aws_vpn_connection.main[0].id
+  vpn_connection_id      = aws_vpn_connection.main.id
 }
 
+# Enable route propagation
 resource "aws_vpn_gateway_route_propagation" "main" {
-  count          = local.vgw_id != "" ? 1 : 0
   vpn_gateway_id = local.vgw_id
   route_table_id = var.route_table_id
 }
