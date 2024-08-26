c6e087919d65a7fdfbe336f9666560576337d43f
Mohamed Sahraoui <Belhadj.boumid@gmail.com>
2024-08-26 16:12:08 +0100

    Update variables.tf

--- modules/vpn/variables.tf
+++ modules/vpn/variables.tf
@@ -1,35 +1,29 @@
 variable "vpc_id" {
-  description = "The ID of the VPC"
+  description = "ID of the VPC"
   type        = string
 }
 
 variable "vpc_name" {
-  description = "The name of the VPC"
+  description = "Name of the VPC"
   type        = string
 }
 
 variable "bgp_asn" {
-  description = "The BGP ASN number for the Customer Gateway"
+  description = "BGP ASN for the Customer Gateway"
   type        = number
 }
 
 variable "cgw_eip" {
-  description = "The Elastic IP address for the Customer Gateway"
+  description = "Elastic IP for the Customer Gateway"
   type        = string
 }
 
 variable "client_vpc_cidr" {
-  description = "The CIDR block for the client VPC"
+  description = "CIDR block of the client VPC"
   type        = string
 }
 
 variable "route_table_id" {
-  description = "The ID of the route table for VPN Gateway route propagation"
+  description = "ID of the route table for VPN route propagation"
   type        = string
-}
-
-variable "create_vgw" {
-  description = "Whether to create a new VPN Gateway"
-  type        = bool
-  default     = false
 }
