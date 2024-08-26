a05d5d675ecd9bb8facaa580f0cd0bf979f53a5e
Mohamed Sahraoui <Belhadj.boumid@gmail.com>
2024-08-26 16:11:13 +0100

    Update variables.tf

--- modules/app_vpc/variables.tf
+++ modules/app_vpc/variables.tf
@@ -9,17 +9,18 @@ variable "vpc_name" {
 }
 
 variable "public_subnet_count" {
-  description = "Number of public subnets to create"
+  description = "Number of public subnets"
   type        = number
-  default     = 1
+  default     = 2
 }
 
 variable "private_subnet_count" {
-  description = "Number of private subnets to create"
+  description = "Number of private subnets"
   type        = number
+  default     = 2
 }
 
 variable "client_vpc_cidr" {
-  description = "CIDR block of the Client VPC"
+  description = "CIDR block of the client VPC"
   type        = string
 }
