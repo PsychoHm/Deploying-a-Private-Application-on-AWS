13fc8b5fc7d49becbf02edea5708be9230cfa43f
Mohamed Sahraoui <Belhadj.boumid@gmail.com>
2024-08-26 16:11:50 +0100

    Update outputs.tf

--- modules/vpn/outputs.tf
+++ modules/vpn/outputs.tf
@@ -1,14 +1,9 @@
-output "vgw_id" {
-  description = "The ID of the VPN Gateway"
-  value       = local.vgw_id
-}
-
-output "cgw_id" {
-  description = "The ID of the Customer Gateway"
-  value       = aws_customer_gateway.cgw.id
-}
-
 output "vpn_connection_id" {
   description = "The ID of the VPN Connection"
   value       = aws_vpn_connection.main.id
+}
+
+output "vgw_id" {
+  description = "The ID of the VPN Gateway"
+  value       = aws_vpn_gateway.vgw.id
 }
